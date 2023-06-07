51e8c852a28b1917ae287457072ad2079c49638e
peterkmissionready <131961491+peterkmissionready@users.noreply.github.com>
2023-06-08 10:06:38 +1200

    Added my part to this Mission, which answers all of Tasks 5,6 and 9.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const router = require('./routes/index');
 const Laptop = require('./models/laptop');
+const LaptopType = require('./models/laptopType');
+const Message = require('./models/Message');
 const cors = require('cors');
+
+var util = require('util');
 
 
 dotenv.config();
@@ -26,6 +30,22 @@ app.get('/laptops', async (req, res) => {
   }
 });
 
+app.get('/laptop_types', async (req, res) => {
+  const params = {} // req.query
+  try {
+    const laptopTypes = await LaptopType.find(params);
+
+    if (!laptopTypes) {
+      return res.status(404).json({ message: 'laptop not found' });
+    }
+
+    res.json(laptopTypes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
 router.get('/api/items', async (req, res) => {
   try {
     const items = await Item.find();
@@ -40,4 +60,35 @@ app.get('/add', async (req, res) => {
   
 });
 
+app.get('/', async (req, res) => {
+  const params = {} // req.query
+  try {
+    const messageX = await Message.find(params);
+
+    if (!messageX) {
+      return res.status(404).json({ message: 'message not found' });
+    }
+    let message=messageX[0].message;
+    res.json({message});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
+
+app.get('/messages', async (req, res) => {
+  const params = {} // req.query
+  try {
+    const message = await Message.find(params);
+
+    if (!message) {
+      return res.status(404).json({ message: 'message not found' });
+    }
+
+    res.json({message});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+});
 module.exports = app;
--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -3,16 +3,21 @@ import React from 'react';
 import { Routes, Route, Outlet} from 'react-router-dom';
 
 import Page0 from './Page0';
+// import Page1 from './Page1';
 import LaptopResults from './LaptopResults';
 import Page2 from './Page2';
+import Index from './index';
 
 function Body() {
   return (
     <div>
       <Routes>
         <Route exact path="/page0" element={<Page0/>} />
+        {/* <Route exact path="/page1" element={<Page1/>} /> */}
         <Route path="/business_laptops" element={<LaptopResults />} />
         <Route path="/page2" element={<Page2/>} />
+        <Route path="/Index" element={<Index/>} />
+        <Route path="/" element={<Index/>} />
       </Routes>
       <Outlet />
     </div>
